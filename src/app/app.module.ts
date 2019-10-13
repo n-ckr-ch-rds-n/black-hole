4cf7f8638d3aa0f6279d87f23eca1585d1d1d504
Nick <nick.richardson@rezonence.com>
2019-10-13 16:29:31 +0100

    Add text input

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,22 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
-import { AppComponent } from './app.component';
-import { BackDropComponent } from './back-drop/back-drop.component';
+import {BrowserModule} from "@angular/platform-browser";
+import {NgModule} from "@angular/core";
+import {AppComponent} from "./app.component";
+import {BackDropComponent} from "./back-drop/back-drop.component";
+import {InputComponent} from "./input/input.component";
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+import {MatFormFieldModule, MatInputModule} from "@angular/material";
 
 @NgModule({
   declarations: [
     AppComponent,
-    BackDropComponent
+    BackDropComponent,
+    InputComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatFormFieldModule,
+    MatInputModule
   ],
   providers: [],
   bootstrap: [AppComponent]
