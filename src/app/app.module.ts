b35f3a31add6011ec883df418fcc5a71b6bff540
Nick <nick.richardson@rezonence.com>
2019-10-13 15:51:11 +0100

    Set shifting background colour dynamically

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { BackDropComponent } from './back-drop/back-drop.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BackDropComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/back-drop/back-drop.component.ts
@@ -0,0 +1,38 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+  selector: "app-back-drop",
+  templateUrl: "./back-drop.component.html",
+  styleUrls: ["./back-drop.component.scss"]
+})
+export class BackDropComponent implements OnInit {
+
+  rgbValues: number[];
+  rgbString: string;
+
+  constructor() {}
+
+  setColourValues(values: number[]): number[] {
+    return values.map(colour => {
+      return colour === 255
+        ? colour - 1
+        : colour === 0
+          ? colour + 1
+          : colour + this.plusOrMinusOne();
+    });
+  }
+
+  plusOrMinusOne() {
+    const index = Math.floor(Math.random() * Math.floor(2));
+    return [-1, 1][index];
+  }
+
+  ngOnInit() {
+    this.rgbValues = [0, 0, 0];
+    setInterval(() => {
+      this.rgbValues = this.setColourValues(this.rgbValues);
+      this.rgbString = `rgb(${this.rgbValues[0]}, ${this.rgbValues[1]}, ${this.rgbValues[2]})`;
+      }, 100);
+  }
+
+}
