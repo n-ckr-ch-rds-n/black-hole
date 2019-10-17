da30ace4fb98833a55432f0ad98f3b1c776920b9
nick_richardson <nick.richardson@rezonence.com>
2019-10-17 16:33:57 +0100

    Set up unauth AWS creds

--- src/app/band-name-service/band-name-service.ts
+++ src/app/band-name-service/band-name-service.ts
@@ -1,30 +1,28 @@
-import * as AWS from "aws-sdk";
-import * as uuidv1 from "uuid/v1";
-import {DocumentClient} from "aws-sdk/lib/dynamodb/document_client";
+import * as AWS from 'aws-sdk';
+import * as uuidv1 from 'uuid/v1';
+import {DocumentClient} from 'aws-sdk/lib/dynamodb/document_client';
 import PutItemInput = DocumentClient.PutItemInput;
-import {environment} from "../../environments/environment";
-import {Injectable} from "@angular/core";
+import {environment} from '../../environments/environment';
+import {Injectable} from '@angular/core';
 
 @Injectable()
 export class BandNameService {
   client: DocumentClient;
 
   constructor() {
-    const devConfig = {region: "us-east-1"};
-    const prodConfig = {...devConfig,
-      accessKeyId: process.env.ACCESS_KEY_ID,
-      secretAccessKey: process.env.SECRET_ACCESS_KEY};
-    const config = environment.production ? prodConfig : devConfig;
-    AWS.config.update(config);
+    AWS.config.region = 'us-east-1';
+    AWS.config.credentials = new AWS.CognitoIdentityCredentials({
+      IdentityPoolId: 'us-east-1:54efc993-0bcc-4bda-85fe-79d1126c1566'
+    });
     this.client = new AWS.DynamoDB.DocumentClient();
   }
 
   toParams(name: string): PutItemInput {
     return {
-      TableName: "band-names",
+      TableName: 'band-names',
       Item: {
-        "id": uuidv1(),
-        "name": name
+        'id': uuidv1(),
+        'name': name
       }
     };
   }
