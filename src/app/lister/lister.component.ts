cfa241c0d0d424a3a8a81d33147f598f7dcfee49
Nick <nick.richardson@rezonence.com>
2019-11-10 17:25:42 +0000

    Prevent admin from voting

--- src/app/lister/lister.component.ts
+++ src/app/lister/lister.component.ts
@@ -31,7 +31,7 @@ export class ListerComponent implements OnInit {
   }
 
   async recordVote(): Promise<void> {
-    for (const name of this.ratableNames.slice(0, 1)) {
+    for (const name of this.ratableNames) {
       const dbEntry = await this.bandNameService.getBandNameById(name.id);
       const rating = dbEntry.rating ? dbEntry.rating + name.rating : name.rating;
       await this.bandNameService.updateEntryWithRating(dbEntry.id, rating);
@@ -43,7 +43,9 @@ export class ListerComponent implements OnInit {
   }
 
   async vote() {
-    if (this.voterService.user.voted) {
+    if (this.voterService.user.name === "Admin") {
+      this.snackBar.open(`Apols you can't vote, ${this.voterService.user.name}`, "Ok");
+    } else if (this.voterService.user.voted) {
       this.snackBar.open(`Apols ${this.voterService.user.name}, you have already voted`, "Ok");
     } else {
       await this.recordVote();
