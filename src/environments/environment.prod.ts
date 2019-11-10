f5ffabc77193bb61ba0e75d4f0f8370b243645a2
Nick <nick.richardson@rezonence.com>
2019-11-10 18:06:10 +0000

    add voter table to prod env vars

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,7 @@
 export const environment = {
   production: true,
-  awsRegion: 'us-east-1',
-  identityPoolId: 'us-east-1:54efc993-0bcc-4bda-85fe-79d1126c1566',
-  bandNameTableName: 'band-names',
+  awsRegion: "us-east-1",
+  identityPoolId: "us-east-1:54efc993-0bcc-4bda-85fe-79d1126c1566",
+  bandNameTableName: "band-names",
+  voterTableName: "voters"
 };
